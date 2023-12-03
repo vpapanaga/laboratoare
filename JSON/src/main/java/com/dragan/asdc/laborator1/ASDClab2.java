3a01521b69aeac6a416212232be286d295689d4f
Dragan Marcu <marcu.dragan@marcodor.com>
2023-12-03 19:05:53 +0200

    add ASDClab2

---
+++ JSON/src/main/java/com/dragan/asdc/laborator1/ASDClab2.java
@@ -0,0 +1,228 @@
+package com.dragan.asdc.laborator1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ASDClab2 {
+    static void bubbleSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                // Compare adjacent elements
+                if (array[j] > array[j+1]) {
+                    // Swap them if they are in the wrong order
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Metoda rapidă de sortare (Quick Sort)
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    // Metoda Shell de sortare (Shell Sort)
+    public static void shellSort(int[] arr) {
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
+            }
+        }
+    }
+
+    // Metoda Heap (piramidală, arborescentă) de sortare (Heap Sort)
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Metoda de sortare prin interclasare (Merge Sort)
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            leftArr[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MyFileReader myFileReader = new MyFileReader("data.json");
+        List<Person> persons = myFileReader.read();
+        int[] arr = new int[persons.size()]; // Create an array to store ids
+        int i = 0;
+        for (Person p: persons) {
+            arr[i++]= (int) p.getId();
+           // System.out.println("p.getId() = " + p.getId());
+        }
+        searchExample(arr);
+    }
+
+    private static void searchExample(int[] arr) {
+        // Selection Sort
+        bubbleSort(arr);
+        System.out.println("Bubble Sort: " + Arrays.toString(arr));
+
+        // Selection Sort
+        insertionSort(arr);
+        System.out.println("Insertion Sort: " + Arrays.toString(arr));
+
+        // Selection Sort
+        selectionSort(arr.clone());
+        System.out.println("Selection Sort: " + Arrays.toString(arr));
+
+        // Quick Sort
+        quickSort(arr.clone(), 0, arr.length - 1);
+        System.out.println("Quick Sort: " + Arrays.toString(arr));
+
+        // She'll Sort
+        shellSort(arr.clone());
+        System.out.println("Shell Sort: " + Arrays.toString(arr));
+
+        // Heap Sort
+        heapSort(arr.clone());
+        System.out.println("Heap Sort: " + Arrays.toString(arr));
+
+        // Merge Sort
+        mergeSort(arr.clone(), 0, arr.length - 1);
+        System.out.println("Merge Sort: " + Arrays.toString(arr));
+    }
+
+    static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+
